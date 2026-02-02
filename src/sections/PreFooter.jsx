2f35626f4858a9dab4871fe7ba88a3ed888095fe
Álex Bryan <bryannascimentopl800@gmail.com>
2026-02-02 17:08:12 -0300

    style: fix height and gradient

--- src/sections/PreFooter.jsx
+++ src/sections/PreFooter.jsx
@@ -4,28 +4,33 @@ import Divider from '../components/Divider';
 
 const PreFooter = () => {
   return (
-    <section id="contato" className="relative h-screen w-full overflow-hidden">
-      {/* 1. Imagem de Fundo */}
-      <div className="w-ful absolute inset-0 h-full">
+    <section
+      id="contato"
+      className="relative h-[500px] w-full overflow-hidden lg:h-[600px]"
+    >
+      {/* Imagem de Fundo */}
+      <div className="absolute inset-0 h-full w-full">
         <img
           src={footerImg}
-          alt="Imagem da faixada de um imóvel"
+          alt="Imagem da fachada de um imóvel"
           className="h-full w-full object-cover object-center"
         />
       </div>
 
-      {/* 2. Gradiente / divisa */}
-      <div className="absolute inset-0 bg-gradient-to-t from-hkn-black to-hkn-black/30"></div>
-      <Divider className="absolute bottom-0 left-0 z-10" />
+      {/* O Gradiente Mágico*/}
+      <div className="absolute inset-0 bg-gradient-to-t from-hkn-black via-hkn-black/60 to-black/20"></div>
 
-      {/* 3. Conteúdo Principal */}
+      {/* Divider separando visualmente */}
+      <Divider className="absolute bottom-0 left-0 z-20" />
+
+      {/* Conteúdo */}
       <div className="container relative z-10 mx-auto flex h-full flex-col justify-center px-6 md:px-12 lg:px-20">
-        <div className="flex flex-col items-center justify-center gap-4 text-center">
-          <h1 className="text-4xl font-semibold text-hkn-white md:text-4xl lg:text-5xl">
+        <div className="flex flex-col items-center justify-center gap-6 text-center">
+          <h1 className="text-3xl font-semibold leading-tight text-white md:text-4xl lg:text-5xl">
             Vamos tirar seu <br /> projeto do papel?
           </h1>
 
-          <p className="max-w-md text-center text-base text-gray-300 md:max-w-lg md:text-lg">
+          <p className="max-w-md text-center text-sm text-gray-300 md:max-w-lg md:text-lg">
             Evite dores de cabeça e garanta a melhor engenharia para o seu
             imóvel. Fale diretamente comigo.
           </p>
